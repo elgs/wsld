c232d9b7525c02c64261c91faeacb5bf5fa22efe
Elgs Qian Chen <elgs1980@hotmail.com>
2017-11-15 12:22:19 -0700

    Update.

--- wsld.go
+++ wsld.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/elgs/wsl"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	version := flag.Bool("v", false, "prints version")
 	confFile := flag.String("c", "/etc/myapp/app.json", "Configration file path.")
 	flag.Parse()
+
+	if *version {
+		fmt.Println("Alpha 20171115")
+		os.Exit(0)
+	}
 
 	wsld, err := wsl.New(*confFile)
 	if err != nil {
