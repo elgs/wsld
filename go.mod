93892dac1cd9eb47e171598cb097407c033b2ea1
Qian Chen <elgs1980@hotmail.com>
2022-07-05 04:12:25 -0700

    Update.

--- go.mod
+++ go.mod
@@ -1,17 +1,19 @@
 module github.com/elgs/wsld
 
-go 1.17
+go 1.18
+
+replace github.com/elgs/wsl => ../wsl
 
 require (
-	github.com/elgs/wsl v0.0.0-20210417213411-26b77158ad64
+	github.com/elgs/wsl v0.0.0-20220703090108-3d5446164558
 	github.com/go-sql-driver/mysql v1.6.0
 )
 
 require (
-	github.com/elgs/gojq v0.0.0-20201120033525-b5293fef2759 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/elgs/gosplitargs v0.0.0-20161028071935-a491c5eeb3c8 // indirect
 	github.com/elgs/gosqljson v0.0.0-20160403005647-027aa4915315 // indirect
-	github.com/elgs/gostrgen v0.0.0-20161222160715-9d61ae07eeae // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/elgs/gostrgen v0.0.0-20220325073726-0c3e00d082f6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- wsld.go
+++ wsld.go
@@ -1,38 +1,34 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
 	"log"
 
 	"github.com/elgs/wsl"
 	"github.com/elgs/wsl/interceptors"
-	"github.com/elgs/wsl/jobs"
 	"github.com/elgs/wsl/scripts"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	app, err := wsl.NewWithConfigPath("/etc/wsld.json")
+
+	confPath := flag.String("c", "wsld.json", "configration file path")
+	flag.Parse()
+	confBytes, err := ioutil.ReadFile(*confPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
+	config, err := wsl.NewConfig(confBytes)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	app := wsl.NewApp(config)
 
 	// optionally load built in user management interceptors and scripts, and jobs
 	scripts.LoadBuiltInScripts(app)
 	interceptors.RegisterBuiltInInterceptors(app)
-	jobs.RegisterBuiltInJobs(app)
-
-	// done manully
-	// wsld.RegisterGlobalInterceptors(&interceptors.AuthInterceptor{})
-	// wsld.RegisterQueryInterceptors("signup", &interceptors.SignupInterceptor{})
-	// ...
-
-	// wsld.Scripts["init"] = scripts.Init
-	// wsld.Scripts["signup"] = scripts.Signup
-	// ...
-
-	// wsld.RegisterJob("clean-trash", CleanPrivateFlags)
-	// ...
 
 	app.Start()
-	wsl.Hook()
 }
